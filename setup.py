6b3fca0694364d6c550aefcfc0fca0ad026f4abb
x1nyuan <yao.ntno@gmail.com>
2017-04-25 01:19:01 +0900

    add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name="app",
+    version="0.1",
+    packages=find_packages(),
+    test_suite='tests'
+)
