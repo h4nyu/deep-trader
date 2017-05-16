ba0a424dbaa6a40421d0bab6f9124eb7717a76d5
x1nyuan <yao.ntno@gmail.com>
2017-05-16 22:46:42 +0900

    pip install 確認

--- Makefile
+++ Makefile
@@ -1,19 +1,26 @@
 # -*- coding: utf-8 -*-
 # Need virtualenv
-.PHONY: all clean build rebuild
+.PHONY: all clean build database pip restore
 
-all: env build
+all: install
 	@echo "build finished..."
 
-build:env
-	pip-compile  dev-requirements.in
+install:
+	pip install -e .
+
+install_dev:
+	pip install -e .[dev]
+
+test:
+	py.test
+
+update_pacakege:
+	pip install pip-tools
+	pip-compile requirements.in
 	pip-compile dev-requirements.in
 	pip-sync requirements.txt dev-requirements.txt
 
-test:
-	python setup.py test
 
-env:
-	pip install pip-tools
-	ln -sf $(shell pwd)/app $(shell pwd)/tests/app
 
+
+			
--- setup.py
+++ setup.py
@@ -1,46 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
+import pip
+from pip.req import parse_requirements
 from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
 
+install_reqs = parse_requirements('requirements.txt',
+                                  session=pip.download.PipSession)
+install_reqs = [str(ir.req) for ir in install_reqs]
 
-class PyTest(TestCommand):
-
-    user_options = [
-        ('pytest-args=', 'a', 'Arguments for pytest'),
-    ]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_target = []
-        self.pytest_args = []
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
-
-
-setup_requires = [
-    'pytest'
-]
-
-tests_require = [
-    'pytest-timeout',
-    'pytest'
-]
+dev_reqs = parse_requirements('dev-requirements.txt',
+                              session=pip.download.PipSession)
+dev_reqs = [str(ir.req) for ir in dev_reqs]
 
 
 setup(
-    name="app",
+    name="deep_trader",
     version="0.1",
+    description="TODO",
+    author='Fintech Group',
+    author_email='yao.ntno@gmail.com',
+    url='TODO',
+    license="TODO",
     packages=find_packages(),
-    cmdclass={'test': PyTest}
+    setup_requires=[
+        "pip-tools"
+        ],
+    install_requires=install_reqs,
+    extras_require={
+        'dev': dev_reqs,
+    }
 )
